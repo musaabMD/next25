2e76f63637a892dbf32f5fc30e8d0f8a8c3f1d7e
musaabMD <mousab.r@outlook.com>
2024-09-02 18:20:55 +0300

    “new”

--- app/page.js
+++ app/page.js
@@ -1,10 +1,15 @@
 import Link from "next/link";
 import ButtonSignin from "@/components/ButtonSignin";
 import Header from "@/components/Header";
+import { Suspense } from "react";
 export default function Page() {
   return (
     <>
+    <Suspense>
+
  <Header/>
+ </Suspense>
+
     </>
   );
 }
