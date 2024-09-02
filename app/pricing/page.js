38a4ee91a8cf39ea299dde53db3ea2df606c3e5b
musaabMD <mousab.r@outlook.com>
2024-09-02 18:34:41 +0300

    “new”

--- app/pricing/page.js
+++ app/pricing/page.js
@@ -2,13 +2,17 @@ import React from 'react'
 import Header from '@/components/Header'
 import FAQ from '@/components/FAQ'
 import Pricing from '@/components/Pricing'
+import { Suspense } from 'react'
 const page = () => {
   return (
     <>
+    <Suspense>
+
+   
     <Header/>
     <Pricing/>
     <FAQ/>
-    
+    </Suspense>
     
     </>
   )
